3be8e5ed5f1f865e3dceee797e1dd316b4ad24fd
Nilasish <nilasishhalderofficial@gmail.com>
2025-07-26 09:08:27 +0530

    Revert "One last commit"
    This reverts commit c75d62fce0ea06c0668fde34afbf522d49d77793.

--- backend/server.js
+++ backend/server.js
@@ -17,7 +17,7 @@ import authRoutes from './routes/auth.js';
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT || 4000;
+const PORT = process.env.PORT || 5000;
 
 // Rate limiting
 const limiter = rateLimit({
